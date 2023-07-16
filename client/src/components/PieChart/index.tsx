3f23bdf42ac193bd0e489275e638ff3674831eb9
tonyandy5630 <tonyandy456@gmail.com>
2023-07-17 03:16:05 +0700

    test ci/cd

--- client/src/components/PieChart/index.tsx
+++ client/src/components/PieChart/index.tsx
@@ -26,6 +26,7 @@ export default function PieChartComp({
       sm={12}
       md={md}
     >
+      //* test ci/cd
       <Box
         sx={{ width: "100%" }}
         className='flex flex-col items-center justify-center'
