02a34ebdd0eddfd95002c9b67c485bc502258c4c
tonyandy5630 <tonyandy456@gmail.com>
2023-07-21 12:11:14 +0700

    add production api

--- client/src/utils/http.ts
+++ client/src/utils/http.ts
@@ -4,7 +4,10 @@ class Http {
   instance: AxiosInstance;
   constructor() {
     this.instance = axios.create({
-      baseURL: process.env.NEXT_PUBLIC_SERVER_URL,
+      baseURL:
+        process.env.NODE_ENV === "development"
+          ? process.env.NEXT_PUBLIC_SERVER_URL
+          : process.env.NEXT_PUBLIC_PROD_SERVER_URL,
       timeout: 10000,
       withCredentials: true,
       headers: {
